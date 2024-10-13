acee54fc955c74c48fc5ce594509b7bea5bd1af7
Ragavendan <ragavendan7@gmail.com>
2024-10-13 17:28:54 +0530

    added deployment modules

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.VITE_PORT) || 3000, 
+    port: Number(process.env.VITE_PORT) || 3000,
   },
+  base: "authenticate_reactjs",
 });
